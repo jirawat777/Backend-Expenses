44faba6fca865ea890b3ec5cfa1403df4d898042
Jirawat Nuanjun <86031118+jirawat777@users.noreply.github.com>
2022-07-25 01:12:32 +0700

    Update settings.py

--- Expenses/Expenses/settings.py
+++ Expenses/Expenses/settings.py
@@ -49,10 +49,7 @@ INSTALLED_APPS = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "https://example.com",
-    "https://sub.example.com",
-    "http://localhost:8080",
-    "http://127.0.0.1:9000",
+    "http://localhost:5173",
 ]
 
 CORS_ALLOW_METHODS = [
